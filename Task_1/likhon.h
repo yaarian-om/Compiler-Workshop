ecd0d73b3628bdd819ec3b3513afd5496e74df2c
Yaarian <dip.kumar020@gmail.com>
2022-07-20 22:55:37 +0600

    Updating  Compiler Design Task_1 || Commit : 1

---
+++ Task_1/likhon.h
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//
+
+
+/// MAKE SURE YOU DO NOT MAKE THE SAME NAME FUNCTION IN 2 DIFFERENT HEADER FILE IN THE SAME PROJECT
+void TheFunctionName(){
+
+	cout<< "This is Likhon"<<endl;
+}
+
---
+++ Task_1/sudipta.h
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/// MAKE SURE YOU DO NOT MAKE THE SAME NAME FUNCTION IN 2 DIFFERENT HEADER FILE IN THE SAME PROJECT
+void functionName(){
+
+	cout<< "This is Kumar"<<endl;
+}
