3b4f3c063a2c3c7283c4e17907737aec79201b2c
Yaarian <dip.kumar020@gmail.com>
2022-07-22 23:47:20 +0600

    Uploading sudipta.h

--- Task_1/likhon.h
+++ Task_1/likhon.h
@@ -1,12 +1,1 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-//
-
-
-/// MAKE SURE YOU DO NOT MAKE THE SAME NAME FUNCTION IN 2 DIFFERENT HEADER FILE IN THE SAME PROJECT
-void TheFunctionName(){
-
-	cout<< "This is Likhon"<<endl;
-}
-
+#include<iostream>using namespace std;bool Print_Identifiers(){	cout<< "Working";}
--- Task_1/main.cpp
+++ Task_1/main.cpp
@@ -1,15 +1,1 @@
-#include <bits/stdc++.h>
-#include <conio.h>
-#include "sudipta.h"
-#include "likhon.h"
-using namespace std;
-
-/// MAKE SURE YOU DO NOT MAKE THE SAME NAME FUNCTION IN 2 DIFFERENT HEADER FILE IN THE SAME PROJECT
-
-int main()
-{
-	functionName();
-	TheFunctionName();
-
-    getch();
-}
+#include <bits/stdc++.h>#include <conio.h>#include "sudipta.h"#include "likhon.h"using namespace std;/// MAKE SURE YOU DO NOT MAKE THE SAME NAME FUNCTION IN 2 DIFFERENT HEADER FILE IN THE SAME PROJECTint main(){	regularExpresstionMaker("xyz + cd = 5 ;");    getch();}
