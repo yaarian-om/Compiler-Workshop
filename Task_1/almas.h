1f04f67c85ca8d2e94160663e0eda1d7b8634271
Likhon <mdsazzadsiddqiue0@gmail.com>
2022-07-25 12:59:11 +0600

    Add user input

---
+++ Task_1/almas.h
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+string input()
+{
+    cout << "Regular Expression: ";
+    string regularExpression;
+    getline(cin, regularExpression);
+    return regularExpression;
+}
